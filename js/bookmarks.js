b80da6566af147ad07a1e18fd2c382ca6a9c3269
HeleneVeber <veber.helene@gmail.com>
2023-01-17 10:09:22 +0100

    (feat) fetch bookmarks et arborescence

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,28 +1,52 @@
-const gettingBookmarks = chrome.bookmarks.getTree();
-console.log(gettingBookmarks)
+document.getElementById("bookmarks").addEventListener("click", getBookmarks);
 
-function getFirtsItemNode () {
-    gettingBookmarks.then((bookmarksTree)=>{
-        let firtsItemNode = bookmarksTree[0].children;
-        let itemNode = [];
-        for (let i=0; i<firtsItemNode.length; i++) {
-            document.getElementById("title").innerHTML += firtsItemNode[i].title + " ";
-            itemNode.push(firtsItemNode[i]);
-        };
-        console.log(itemNode);
-        return itemNode;
+// getBookmarks() récupère l'intégralite l'arborescence des marques pages de chrome et à partir du noeud principal rècupère les descendants
+console.log(chrome.bookmarks.getTree());
+
+function getBookmarks() {
+    // arborescence principale
+    chrome.bookmarks.getTree((bookmarksTree)=>{
+        //descendants
+        chrome.bookmarks.getChildren(bookmarksTree[0].id, showBookmarks);
     });
 };
 
-getFirtsItemNode ()
+// showBookmarks() est une fonction recursive qui affiche les favoris sur la page HTML et parcours l'arborescence des favoris
 
-
-function getId(item) {
-    let arrayID = [];
-    for (let i in item) {
-        arrayID.push(item[i].id);
-    }
-    console.log
-    return arrayID;
-}
-
+function showBookmarks(bookmarksTree) {
+    document.getElementById("bookmarks").removeEventListener("click", getBookmarks);
+    const mainFolder = document.createElement("ul");
+    //Parcours l'arborescence
+    for(let i=0; i<bookmarksTree.length; i++) {
+        // Si Url existante = affichage
+        if (bookmarksTree[i].url != null) {
+            const fileLi = document.createElement("li");
+            const aLink = document.createElement("a");
+            const aLinkText = document.createTextNode(bookmarksTree[i].title);
+            aLink.href = bookmarksTree[i].url;
+            aLink.appendChild(aLinkText);
+            fileLi.appendChild(aLink);
+            mainFolder.appendChild(fileLi);
+        }
+        // Sinon c'est un folder = affichage du folder + recursivité pour trouver l'Url
+        else {
+            const subFolder = document.createElement("li");
+            const openButton = document.createElement("button");
+            const folderImg = document.createElement("img");
+            const fileLiText = document.createTextNode(bookmarksTree[i].title);
+            folderImg.src = "img/folderIcon/folder.png";
+            subFolder.id = "level"+bookmarksTree[i].id;
+            openButton.id = "button"+bookmarksTree[i].id;
+            openButton.appendChild(folderImg);
+            openButton.appendChild(fileLiText);
+            subFolder.appendChild(openButton);
+            mainFolder.appendChild(subFolder);
+            // Recursivité pour parcourrir le folder en question
+            
+            //document.getElementById(`button${bookmarksTree[i].id}`).addEventListener("click", () => {})
+            chrome.bookmarks.getChildren(bookmarksTree[i].id, showBookmarks);
+        }; 
+        const parent = document.getElementById("level"+bookmarksTree[i].parentId);
+        parent.appendChild(mainFolder);
+    };
+};
