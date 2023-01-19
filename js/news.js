84e27565929f7e4823201f7f939a03abddbe0384
Gabrielparizet <parizetgabriel@gmail.com>
2023-01-19 14:38:10 +0100

    (style) Carousel

--- js/news.js
+++ js/news.js
@@ -1,5 +1,5 @@
 // La fonction fetchNews() va réaliser un fetch sur l'api vers newsAPI afin d'obtenir des titres de nouvelles du jour dans certains médias français.
-const newsUrl = "https://newsapi.org/v2/top-headlines?country=fr&apiKey=459b0b59249043c583f1317f23d5fbc4"
+const newsUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=459b0b59249043c583f1317f23d5fbc4"
 
 
 const fetchNews = async (url) => {
@@ -7,6 +7,7 @@ const fetchNews = async (url) => {
         const response = await fetch(url, {mode: 'cors'}
         );
         let data = await response.json();
+        console.log(data);
         showNews(data);
         return data;
     }
@@ -15,40 +16,33 @@ const fetchNews = async (url) => {
     }
 }
 
-
-const myCarouselElement = document.querySelector('#myCarousel')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
-
-
 function showNews (data){
     const divActive = document.createElement("div");
     divActive.className = "carousel-item active";
     const imgActive = document.createElement("img");
     imgActive.src = data.articles[0].urlToImage;
     imgActive.className = "d-block w-100";
+    imgActive.className = "styleImage"
     const divCaptionActive = document.createElement("div");
     divCaptionActive.className = "carousel-caption d-none d-md-block";
     const pTitleActive = document.createElement("p");
-    pTitleActive.innerHTML = `<a href="${data.articles[0].url}">${data.articles[0].title}</a>`
+    pTitleActive.innerHTML = `<a href="${data.articles[0].url}" class="styleTitle" target="_blank">${data.articles[0].title}</a>`
     divCaptionActive.appendChild(pTitleActive);
     divActive.appendChild(imgActive);
     divActive.appendChild(divCaptionActive);
     document.getElementById("carousel-inner").appendChild(divActive);
-    for (let i = 1; i <= 10; i++){
+    for (let i = 1; i < 10; i++){
         if (data.articles[i].urlToImage !== null){
             const div = document.createElement("div");
             div.className = "carousel-item";
             const img = document.createElement("img");
             img.src = data.articles[i].urlToImage;
             img.className = "d-block w-100";
+            img.className = "styleImage"
             const divCaption = document.createElement("div");
             divCaption.className = "carousel-caption d-none d-md-block";
             const pTitle = document.createElement("p");
-            pTitle.innerHTML = `<a href="${data.articles[i].url}">${data.articles[i].title}</a>`
+            pTitle.innerHTML = `<a href="${data.articles[i].url}" class="styleTitle" target="_blank">${data.articles[i].title}</a>`
             divCaption.appendChild(pTitle);
             div.appendChild(img);
             div.appendChild(divCaption);
@@ -57,4 +51,4 @@ function showNews (data){
     }
 }
 
-// fetchNews(newsUrl);
+fetchNews(newsUrl);
