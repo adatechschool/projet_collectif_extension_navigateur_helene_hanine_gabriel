71635029d9a5c4c6784688b8f21c493a4e30e860
“Hanineali” <hanineali.bc@gmail.com>
2023-01-19 09:22:23 +0100

    (style) todolist with html modification

--- js/todo.js
+++ js/todo.js
@@ -17,15 +17,16 @@ function loadTasks(){
     // insert every todo item in the li
     let userVal = document.createElement('input');
     userVal.value = todos[i].mytask;
+    userVal.type="text"
     userVal.id = i;
-
      // add an id for every element to be able to refer to it
     nodeLi.id = i;
 
     // create the input element
     let check = document.createElement("INPUT");
     Object.assign(check, {
-      type : "checkbox"
+      type : "checkbox",
+      id:"checkbox"
     })
 
     // create delete button element
@@ -37,7 +38,7 @@ function loadTasks(){
 
      // create edit button element
     let editBtn = document.createElement("button");
-    editBtn.textContent= "Edit";
+    editBtn.textContent= "E";
     Object.assign(editBtn, {
       type : "button",
     })
@@ -45,7 +46,7 @@ function loadTasks(){
     //add the event listener to the button
     deleteBtn.addEventListener("click",() => deleteItem(nodeLi));
 
-    //editBtn.addEventListener('click', () => editTask(nodeLi, userVal));
+    editBtn.addEventListener('click', () => editTask(userVal));
 
     // append input, deletebtn to nodeLI (li element)
     nodeLi.appendChild(check);
@@ -75,16 +76,14 @@ function addTask(){
     
   }
 
-/* function editTask(elem, elem2){
+  function editTask(elem){
 
-    let newTask = document.getElementById(elem2.id).value;
-    console.log(newTask)
-
-    todos.splice(elem.id,1, newTask);
-    todos.push({ mytask: newTask});
+    let newTask = elem.value
+    //console.log(newTask)
+   
+    todos.splice(elem.id, 1 , {mytask: newTask});
     localStorage.setItem('todos', JSON.stringify(todos));
-
-}  */
+}  
 
 // create the delete function that removes the nodeLI when the button in the node is clicked
 function deleteItem(elem){
